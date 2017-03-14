e291056de15d6cfa72cf2030296e29126a7e4b55
mo <mo@mos-MacBook-Pro.local>
2017-03-14 19:15:06 +0800

    test

--- example/main.go
+++ example/main.go
@@ -9,7 +9,7 @@ import (
 
 func testRRule() {
 	fmt.Println("Daily, for 10 occurrences.")
-	r, _ := rrule.NewRRule(&rrule.ROption{
+	r, _ := rrule.NewRRule(rrule.ROption{
 		Freq:    rrule.DAILY,
 		Count:   10,
 		Dtstart: time.Date(1997, 9, 2, 9, 0, 0, 0, time.Local)})
@@ -18,7 +18,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nDaily until December 24, 1997")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:    rrule.DAILY,
 		Dtstart: time.Date(1997, 9, 2, 9, 0, 0, 0, time.Local),
 		Until:   time.Date(1997, 12, 24, 0, 0, 0, 0, time.Local)})
@@ -27,7 +27,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery other day, 5 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:     rrule.DAILY,
 		Interval: 2,
 		Count:    5,
@@ -37,7 +37,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery 10 days, 5 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:     rrule.DAILY,
 		Interval: 10,
 		Count:    5,
@@ -47,7 +47,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEveryday in January, for 3 years.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.YEARLY,
 		Bymonth:   []int{int(time.January)},
 		Byweekday: []rrule.Weekday{rrule.MO, rrule.TU, rrule.WE, rrule.TH, rrule.FR, rrule.SA, rrule.SU},
@@ -58,7 +58,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nSame thing, in another way.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:    rrule.DAILY,
 		Bymonth: []int{int(time.January)},
 		Dtstart: time.Date(1998, 1, 1, 9, 0, 0, 0, time.Local),
@@ -68,7 +68,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nWeekly for 10 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:    rrule.WEEKLY,
 		Count:   10,
 		Dtstart: time.Date(1997, 9, 2, 9, 0, 0, 0, time.Local)})
@@ -77,7 +77,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery other week, 6 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:     rrule.WEEKLY,
 		Interval: 2,
 		Count:    6,
@@ -87,7 +87,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nWeekly on Tuesday and Thursday for 5 weeks.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.WEEKLY,
 		Count:     10,
 		Wkst:      rrule.SU,
@@ -98,7 +98,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery other week on Tuesday and Thursday, for 8 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.WEEKLY,
 		Interval:  2,
 		Count:     8,
@@ -110,7 +110,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nMonthly on the 1st Friday for ten occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.MONTHLY,
 		Count:     10,
 		Byweekday: []rrule.Weekday{rrule.FR.Nth(1)},
@@ -120,7 +120,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery other month on the 1st and last Sunday of the month for 10 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.MONTHLY,
 		Interval:  2,
 		Count:     10,
@@ -131,7 +131,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nMonthly on the second to last Monday of the month for 6 months.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.MONTHLY,
 		Count:     6,
 		Byweekday: []rrule.Weekday{rrule.MO.Nth(-2)},
@@ -141,7 +141,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nMonthly on the third to the last day of the month, for 6 months.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:       rrule.MONTHLY,
 		Count:      6,
 		Bymonthday: []int{-3},
@@ -151,7 +151,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nMonthly on the 2nd and 15th of the month for 5 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:       rrule.MONTHLY,
 		Count:      5,
 		Bymonthday: []int{2, 15},
@@ -161,7 +161,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nMonthly on the first and last day of the month for 3 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:       rrule.MONTHLY,
 		Count:      5,
 		Bymonthday: []int{-1, 1},
@@ -171,7 +171,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery 18 months on the 10th thru 15th of the month for 10 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:       rrule.MONTHLY,
 		Interval:   18,
 		Count:      10,
@@ -182,7 +182,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery Tuesday, every other month, 6 occurences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.MONTHLY,
 		Interval:  2,
 		Count:     6,
@@ -193,7 +193,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nYearly in June and July for 4 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:    rrule.YEARLY,
 		Count:   4,
 		Bymonth: []int{6, 7},
@@ -203,7 +203,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery 3rd year on the 1st, 100th and 200th day for 4 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.YEARLY,
 		Count:     4,
 		Interval:  3,
@@ -214,7 +214,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery 20th Monday of the year, 3 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.YEARLY,
 		Count:     3,
 		Byweekday: []rrule.Weekday{rrule.MO.Nth(20)},
@@ -224,7 +224,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nMonday of week number 20 (where the default start of the week is Monday), 3 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.YEARLY,
 		Count:     3,
 		Byweekno:  []int{20},
@@ -235,7 +235,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nThe week number 1 may be in the last year.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.WEEKLY,
 		Count:     3,
 		Byweekno:  []int{1},
@@ -246,7 +246,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nAnd the week numbers greater than 51 may be in the next year.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.WEEKLY,
 		Count:     3,
 		Byweekno:  []int{52},
@@ -257,7 +257,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nOnly some years have week number 53:")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.WEEKLY,
 		Count:     3,
 		Byweekno:  []int{53},
@@ -268,7 +268,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery Friday the 13th, 4 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:       rrule.YEARLY,
 		Count:      4,
 		Byweekday:  []rrule.Weekday{rrule.FR},
@@ -279,7 +279,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery four years, the first Tuesday after a Monday in November, 3 occurrences (U.S. Presidential Election day).")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:       rrule.YEARLY,
 		Interval:   4,
 		Count:      3,
@@ -292,7 +292,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nThe 3rd instance into the month of one of Tuesday, Wednesday or Thursday, for the next 3 months.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.MONTHLY,
 		Count:     3,
 		Byweekday: []rrule.Weekday{rrule.TU, rrule.WE, rrule.TH},
@@ -303,7 +303,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nThe 2nd to last weekday of the month, 3 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.MONTHLY,
 		Count:     3,
 		Byweekday: []rrule.Weekday{rrule.MO, rrule.TU, rrule.WE, rrule.TH, rrule.FR},
@@ -314,7 +314,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery 3 hours from 9:00 AM to 5:00 PM on a specific day.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:     rrule.HOURLY,
 		Interval: 3,
 		Dtstart:  time.Date(1997, 9, 2, 9, 0, 0, 0, time.Local),
@@ -324,7 +324,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery 15 minutes for 6 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:     rrule.MINUTELY,
 		Interval: 15,
 		Count:    6,
@@ -334,7 +334,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery hour and a half for 4 occurrences.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:     rrule.MINUTELY,
 		Interval: 90,
 		Count:    4,
@@ -344,7 +344,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nEvery 20 minutes from 9:00 AM to 4:40 PM for two days.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:     rrule.MINUTELY,
 		Interval: 20,
 		Count:    48,
@@ -356,7 +356,7 @@ func testRRule() {
 	}
 
 	fmt.Println("\nAn example where the days generated makes a difference because of wkst.")
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.WEEKLY,
 		Interval:  2,
 		Count:     4,
@@ -368,7 +368,7 @@ func testRRule() {
 	}
 
 	fmt.Println()
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.WEEKLY,
 		Interval:  2,
 		Count:     4,
@@ -378,17 +378,18 @@ func testRRule() {
 	for _, v := range r.All() {
 		fmt.Println(v)
 	}
+	fmt.Println(r)
 }
 
 func testRRuleSet() {
 	fmt.Println("\nDaily, for 7 days, jumping Saturday and Sunday occurrences.")
 	set := rrule.Set{}
-	r, _ := rrule.NewRRule(&rrule.ROption{
+	r, _ := rrule.NewRRule(rrule.ROption{
 		Freq:    rrule.DAILY,
 		Count:   7,
 		Dtstart: time.Date(1997, 9, 2, 9, 0, 0, 0, time.Local)})
 	set.RRule(r)
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:      rrule.YEARLY,
 		Byweekday: []rrule.Weekday{rrule.SA, rrule.SU},
 		Dtstart:   time.Date(1997, 9, 2, 9, 0, 0, 0, time.Local)})
@@ -399,7 +400,7 @@ func testRRuleSet() {
 
 	fmt.Println("\nWeekly, for 4 weeks, plus one time on day 7, and not on day 16.")
 	set = rrule.Set{}
-	r, _ = rrule.NewRRule(&rrule.ROption{
+	r, _ = rrule.NewRRule(rrule.ROption{
 		Freq:    rrule.WEEKLY,
 		Count:   4,
 		Dtstart: time.Date(1997, 9, 2, 9, 0, 0, 0, time.Local)})
@@ -412,5 +413,6 @@ func testRRuleSet() {
 }
 
 func main() {
-	fmt.Println("vim-go")
+	testRRule()
+	testRRuleSet()
 }
--- rrule_test.go
+++ rrule_test.go
@@ -1,8 +1,147 @@
 package rrule
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 func TestRRule(t *testing.T) {
+	r, _ := NewRRule(ROption{
+		Freq:    YEARLY,
+		Count:   4,
+		Dtstart: time.Date(1997, 9, 2, 9, 0, 0, 0, time.UTC),
+	})
+	instances := r.All()
+	if len := len(instances); len != 4 {
+		t.Errorf("len(r.All()) = %d, want 4", len)
+	}
+	for i, year := range []int{1997, 1998, 1999, 2000} {
+		want := time.Date(year, 9, 2, 9, 0, 0, 0, time.UTC)
+		if instances[i] != want {
+			t.Errorf("r.All()[i] = %v, want %v", instances[i], want)
+		}
+	}
+	from, to := time.Date(1997, 9, 2, 9, 0, 0, 0, time.UTC), time.Date(2000, 9, 2, 9, 0, 0, 0, time.UTC)
+	instances = r.Between(from, to, true)
+	if instances := r.Between(from, to, true); len(instances) != 4 {
+		t.Errorf("len(r.Between(%v, %v, true)) = %d, want 4", from, to, len(instances))
+	}
+	if instances := r.Between(from, to, false); len(instances) != 2 {
+		t.Errorf("len(r.Between(%v, %v, false)) = %d, want 2", from, to, len(instances))
+	}
+	if instance := r.Before(from, true); instance != from {
+		t.Errorf("r.Before(%v, true) = %v, want %v", from, instance, from)
+	}
+	if instance := r.After(to, true); instance != to {
+		t.Errorf("r.After(%v, true) = %v, want %v", to, instance, to)
+	}
+	if instance := r.After(to, false); !instance.IsZero() {
+		t.Errorf("r.After(%v, false) = %v, want %v", to, instance, time.Time{})
+	}
+}
+
+func TestNewRRule(t *testing.T) {
+	r, e := NewRRule(ROption{Freq: MONTHLY})
+	if seconds := time.Now().Sub(r.dtstart).Seconds(); seconds > 10 {
+		t.Errorf(`time.Now().Sub(r.dtstrt).Seconds() = %f, want < 10`, seconds)
+	}
+
+	r, e = NewRRule(ROption{Bysetpos: []int{367}})
+	if e == nil {
+		t.Errorf("_, e = NewRRule(ROption{Bysetpos:[]int{367}}); e = nil, want error")
+	}
+
+	r, _ = NewRRule(ROption{
+		Freq:    WEEKLY,
+		Dtstart: time.Date(2017, 3, 14, 9, 0, 0, 0, time.UTC),
+	})
+	if len(r.byweekday) != 1 || r.byweekday[0] != TU.weekday {
+		t.Errorf("r.byweekday = %v, want [1]", r.byweekday)
+	}
+
+	r, _ = NewRRule(ROption{
+		Bymonthday: []int{-1},
+		Byweekday:  []Weekday{MO.Nth(1)},
+	})
+	if len(r.bynmonthday) != 1 || r.bynmonthday[0] != -1 {
+		t.Errorf("r.bynmonthday = %v, want [-1]", r.bynmonthday)
+	}
+	if len(r.bynweekday) != 1 || r.bynweekday[0] != MO.Nth(1) {
+		t.Errorf("r.bynweekday = %v, want +1MO", r.bynweekday)
+	}
+}
+
+func TestRRuleIterate(t *testing.T) {
+	r, _ := NewRRule(ROption{
+		Freq:     YEARLY,
+		Dtstart:  time.Date(1997, 9, 2, 0, 0, 0, 0, time.UTC),
+		Byweekno: []int{1, 2, -100},
+	})
+	if value, want := r.All()[0], time.Date(1997, 12, 29, 0, 0, 0, 0, time.UTC); value != want {
+		t.Errorf("r.All()[1] = %v, want %v", value, want)
+	}
+	fmt.Println(r.All()[0])
+
+	r, _ = NewRRule(ROption{
+		Freq:       HOURLY,
+		Dtstart:    time.Date(1997, 9, 1, 23, 0, 0, 0, time.UTC),
+		Until:      time.Date(1997, 9, 3, 11, 0, 0, 0, time.UTC),
+		Bysetpos:   []int{1, -1, 61},
+		Bymonthday: []int{2, 3},
+		Byminute:   []int{0, 1, 2},
+	})
+	if value, want := r.All()[3], time.Date(1997, 9, 2, 1, 2, 0, 0, time.UTC); value != want {
+		t.Errorf("r.All()[1] = %v, want %v", value, want)
+	}
+
+	r, _ = NewRRule(ROption{
+		Freq:       MINUTELY,
+		Dtstart:    time.Date(1997, 9, 2, 23, 0, 0, 0, time.UTC),
+		Count:      4,
+		Bymonthday: []int{2, 4},
+		Byminute:   []int{0},
+		Bysecond:   []int{0},
+	})
+	if value, want := r.All()[3], time.Date(1997, 9, 4, 2, 0, 0, 0, time.UTC); value != want {
+		t.Errorf("r.All()[1] = %v, want %v", value, want)
+	}
+
+	r, _ = NewRRule(ROption{
+		Freq:       SECONDLY,
+		Dtstart:    time.Date(1997, 12, 31, 23, 59, 59, 0, time.UTC),
+		Count:      4,
+		Bysetpos:   []int{1},
+		Bymonthday: []int{31, 2},
+	})
+	if value, want := r.All()[3], time.Date(1998, 1, 2, 0, 0, 2, 0, time.UTC); value != want {
+		t.Errorf("r.All()[3] = %v, want %v", value, want)
+	}
+}
+
+func TestStr(t *testing.T) {
+	str := "FREQ=WEEKLY;DTSTART=20120201T093000Z;INTERVAL=5;WKST=TU;COUNT=2;UNTIL=20130130T230000Z;BYSETPOS=2;BYMONTH=3;BYYEARDAY=95;BYWEEKNO=1;BYDAY=MO,+2FR;BYHOUR=9;BYMINUTE=30;BYSECOND=0;BYEASTER=-1"
+	r, _ := StrToRRule(str)
+	if s := r.String(); s != str {
+		t.Errorf("StrToRRule(%q).String() = %q, want %q", str, s, str)
+	}
+}
+
+func TestInvalidString(t *testing.T) {
+	cases := []string{
+		"",
+		"FREQ",
+		"FREQ=HELLO",
+		"BYMONTH=",
+		"FREQ=WEEKLY;HELLO=WORLD",
+		"FREQ=WEEKLY;BYMONTHDAY=I",
+		"FREQ=WEEKLY;BYDAY=M",
+		"FREQ=WEEKLY;BYDAY=MQ",
+		"FREQ=WEEKLY;BYDAY=+MO",
+	}
+	for _, item := range cases {
+		if _, e := StrToRRule(item); e == nil {
+			t.Errorf("StrToRRule(%q) = nil, want error", item)
+		}
+	}
 }
